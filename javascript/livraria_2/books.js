e01d58c2d821c003a9a13955d7d216933e05d1b4
Renan dos Santos <40153166+renandossantos@users.noreply.github.com>
2025-04-06 14:29:14 +0000

    add `JS-Livraria_2`

---
+++ javascript/livraria_2/books.js
@@ -0,0 +1,41 @@
+let books = [
+  {
+    title: "Pega a Visão",
+    author: "Rick Chesther",
+    alreadyRead: true,
+    imageUrl: "./assets/Rick Chesther - Pega a visão.jpg",
+    link: "https://www.amazon.com.br/gp/product/B07HR27WJX/ref=kinw_myk_ro_title",
+  },
+  {
+    title: "Ponto de Inflexão",
+    author: "Flávio Augusto",
+    alreadyRead: true,
+    imageUrl: "./assets/Flávio Augusto - Ponto de inflexão.jpg",
+    link: "https://www.amazon.com.br/gp/product/B07MZGKTFR/ref=kinw_myk_ro_title",
+  },
+  {
+    title: "Ecce Homo",
+    author: "Friedrich Nietzsche",
+    alreadyRead: false,
+    imageUrl: "./assets/Friedrich Nietzsche - Ecce Homo.jpg",
+    link: "https://www.amazon.com.br/gp/product/B0B2KFN42N/ref=kinw_myk_ro_title",
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    alreadyRead: true,
+    imageUrl: "./assets/George Orwell - 1984.jpg",
+    link: "https://www.amazon.com.br/gp/product/B08T1VXC21/ref=kinw_myk_ro_title",
+  },
+  {
+    title: "Sobre a breviedade da vida",
+    author: "Sêneca",
+    alreadyRead: true,
+    imageUrl: "./assets/Sêneca - Sobre a breviedade da vida.jpg",
+    link: "https://www.amazon.com.br/gp/product/B08VL2HRCD/ref=kinw_myk_ro_title",
+  },
+];
+
+export default function bookDescription() {
+  return books;
+}
---
+++ javascript/livraria_2/scripts.js
@@ -0,0 +1,28 @@
+import bookDescription from "./books.js";
+
+const books = bookDescription();
+
+let bookshelf = document.getElementById("bookshelf");
+
+for (let i = 0; i < books.length; i++) {
+  let book = books[i];
+
+  const newBook = document.createElement("section");
+  newBook.className = "books";
+
+  newBook.innerHTML = `
+<h1 class='book-title'>${book.title}</h1>
+
+<a href='${book.link}'>
+<img src='${book.imageUrl}' alt='${book.title}' class='image'>
+</a>
+
+
+<h2 class='author'>${book.author}</h2>
+<hr>
+<h3 class='status'>${book.alreadyRead ? "✅ Read" : "❌ Not Read"}</h3>
+
+`;
+
+  bookshelf.appendChild(newBook);
+}
